b1990de327e1bc4fca2475ab4402ef97163c5ea0
Francis Kim <frankiekim5@gmail.com>
2020-04-05 21:22:19 -0400

    finished home page

--- brainy-frontend/src/app/categories/categories.component.ts
+++ brainy-frontend/src/app/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { IPhoneme } from './../interfaces/phoneme.interface';
+import { Consonants } from './../constants/consonant.constants';
+import { StateService } from './../services/state.service';
+import { IPhoneme, ICategory } from './../interfaces/phoneme.interface';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { MockVowels, MockConsonants } from '../constants/phoneme.constants';
@@ -12,21 +14,28 @@ export class CategoriesComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private stateService: StateService,
   ) { }
 
+  chosenCategory: ICategory;
   categoryImages: IPhoneme[];
   header: string;
   iterations = [1,2,3,4,5,6,7,8,9];
 
   ngOnInit() {
-    this.route.data
-      .subscribe(d => {
-        this.header = d.header;
-        console.log(this.header);
-        this.header === 'Vowels'
-          ? this.categoryImages = MockVowels
-          : this.categoryImages = MockConsonants;
-      });
+    this.chosenCategory = this.stateService.categoryState;
+    this.stateService.categoryState = undefined;
+    this.header = this.chosenCategory.label;
+    this.categoryImages = this.stateService.getCategory(this.chosenCategory.key);
+    console.log(this.categoryImages);
+    // this.route.data
+    //   .subscribe(d => {
+    //     this.header = d.header;
+    //     console.log(this.header);
+    //     this.header === 'Vowels'
+    //       ? this.categoryImages = MockVowels
+    //       : this.categoryImages = MockConsonants;
+    //   });
   }
 
 }
--- brainy-frontend/src/app/home/home.component.ts
+++ brainy-frontend/src/app/home/home.component.ts
@@ -1,3 +1,6 @@
+import { StateService } from './../services/state.service';
+import { ICategory } from './../interfaces/phoneme.interface';
+import { Categories } from './../constants/category.constants';
 import { Router } from '@angular/router';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
@@ -12,13 +15,21 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private router: Router,
+    private stateService: StateService,
   ) { }
+
+  categoryImgs: ICategory[] = Categories;
 
   ngOnInit() {
   }
 
   onLogout() {
     this.router.navigate([`../landing/`]);
+  }
+
+  onCategoryClick(image: ICategory) {
+    this.stateService.categoryState = image;
+    this.router.navigate(['../category']);
   }
 
   onVowel() {

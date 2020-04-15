198a1bb353a7f083a03c7a0e6956d918ba6ba082
Francis Kim <frankiekim5@gmail.com>
2020-04-15 00:19:09 -0400

    finished user authentication

---
+++ brainy-frontend/src/app/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { UserService } from './../services/user.service';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+      if (!this.userService.isLoggedIn()) {
+        this.router.navigateByUrl('/landing');
+        this.userService.deleteToken();
+        return false;
+      }
+      return true;
+    }
+}
--- brainy-frontend/src/app/services/user.service.ts
+++ brainy-frontend/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { environment } from './../../environments/environment';
 import { User } from './user.model';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,21 +13,54 @@ export class UserService {
     password: '',
   };
 
-  token: string;
-
   constructor(
     private http: HttpClient
   ) { }
 
+  noAuthHeader = { headers: new HttpHeaders({ 'NoAuth': 'True' }) };
+
+  // http
   registerUser(user: User) {
-    return this.http.post(environment.apiBaseUrl + '/register', user);
+    return this.http.post(environment.apiBaseUrl + '/register', user, this.noAuthHeader);
   }
 
   loginUser(user: User) {
-    return this.http.post(environment.apiBaseUrl + '/authenticate', user);
+    return this.http.post(environment.apiBaseUrl + '/authenticate', user, this.noAuthHeader);
   }
 
-  getUser() {
-    return this.http.get(environment.apiBaseUrl + '/userProfile', this.token);
+  getUserProfile() {
+    return this.http.get(environment.apiBaseUrl + '/userProfile');
+  }
+
+  // helper
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  deleteToken() {
+    localStorage.removeItem('token');
+  }
+
+  getUserPayload() {
+    const token = this.getToken();
+    if (token) {
+      const userPayload = atob(token.split('.')[1]);
+      console.log(JSON.parse(userPayload));
+      return JSON.parse(userPayload);
+    } else {
+      return null;
+    }
+  }
+
+  isLoggedIn() {
+    const userPayload = this.getUserPayload();
+    if (userPayload) {
+      return userPayload.exp > Date.now() / 1000;
+    }
+    return false;
   }
 }
